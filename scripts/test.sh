09c8173007a9130cc5d6dcade64062daae0aa6a2
mikereiddigital <mike.reid@justice.gov.uk>
2024-08-28 11:00:59 +0100

    test

--- scripts/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sed -e 's/AWS_SECRET_ACCESS_KEY".*/<REDACTED>/g' \
-    -e 's/AWS_ACCESS_KEY_ID".*/<REDACTED>/g' \
-    -e 's/$AWS_SECRET_ACCESS_KEY".*/<REDACTED>/g' \
-    -e 's/$AWS_ACCESS_KEY_ID".*/<REDACTED>/g' \
-    -e 's/\[id=.*\]/\[id=<REDACTED>\]/g' \
-    -e 's/::[0-9]\{12\}:/::REDACTED:/g' \
-    -e 's/:[0-9]\{12\}:/:REDACTED:/g'
